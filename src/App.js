fa51de44f1ae23053d2eca23d606b9748037ebbf
Eshargh Ghoodjani <e.ghoodjani@gmail.com>
2020-01-06 17:11:56 +0330

    simple react state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  
+  constructor () {
+    super ();
+    this.state = {
+      num : 0 
+    }
+  }
+  
+  
+  render () {
+    return (
+      <div>
+        <h1>Current number is {this.state.num}</h1>
+        <button onClick={this.addone}>Plus one</button>
+      </div>
+    )
+  }
+  
+  addone = () => {
+    const added = this.state.num + 1;
+    this.setState ({num : added});
+  }
+
 }
+
+
 
 export default App;
